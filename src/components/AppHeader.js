73ea8edd18f939408683db86a3ae7a3b29fa2423
Dev Patel <DEVVPATEL2611@GMAIL.COM>
2023-10-04 23:10:44 +0530

    AppHeader created

---
+++ src/components/AppHeader.js
@@ -0,0 +1,18 @@
+import '../styles/NavStyle.css'
+const AppHeader = ()=>{
+
+    return(<>
+        <div className="app-header">
+            <div className="app-header-content">
+            <div>Track Order</div>
+            <div className="login-logout-div">
+                <div>LOG IN</div>
+                <div>|</div>
+                <div>SIGN UP</div>
+            </div>
+            </div>
+            
+        </div>
+    </>)
+}
+export default AppHeader;
