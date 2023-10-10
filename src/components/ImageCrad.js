b2f26f0c75b2e5c26ee785b7dd9a3c16e60105e2
Dev Patel <DEVVPATEL2611@GMAIL.COM>
2023-10-10 21:07:56 +0530

    efa

--- src/components/ImageCrad.js
+++ src/components/ImageCrad.js
@@ -5,15 +5,14 @@ import CardContent from '@mui/material/CardContent';
 import CardMedia from '@mui/material/CardMedia';
 import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
-import AddShoppingCartIcon from '@mui/icons-material/AddShoppingCart';
 import FavoriteBorderIcon from '@mui/icons-material/FavoriteBorder';
 import FavoriteIcon from '@mui/icons-material/Favorite';
-import { ClassNames } from '@emotion/react';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 import { fetcher } from '../helpers';
 
 export default function ImgMediaCard(props) {
   const [isToWishList,setIsToWishList] = useState(false);
+  const navigate = useNavigate();
   // useEffect(()=>{
   //   fetcher("ecommerce/wishlist")
   //   .then((res)=>{console.log(res)
@@ -21,8 +20,9 @@ export default function ImgMediaCard(props) {
   //     })
   //   .catch(err=>console.log(err))
   // },[])
-
+  
   function addtoWishlistAPI(id){
+   if(localStorage.getItem("token")){
     fetcher("ecommerce/wishlist",{
       method:"PATCH",
       body: JSON.stringify({
@@ -33,14 +33,21 @@ export default function ImgMediaCard(props) {
       setIsToWishList(true);
       })
     .catch(err=>console.log(err))
+   }else{
+    return navigate("/login");
+   }
   }
   function removeFromoWishlistAPI(id){
-    fetcher(`ecommerce/wishlist/${id}`,{
-      method:"DELETE"})
-    .then((res)=>{console.log(res)
-      setIsToWishList(false);
-      })
-    .catch(err=>console.log(err))
+    if(localStorage.getItem("token")){
+      fetcher(`ecommerce/wishlist/${id}`,{
+        method:"DELETE"})
+      .then((res)=>{console.log(res)
+        setIsToWishList(false);
+        })
+      .catch(err=>console.log(err))
+    }else{
+      return navigate("/login")
+    }
   }
   
   return (
--- src/pages/CartPage.js
+++ src/pages/CartPage.js
@@ -1,7 +1,69 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import {
+  Paper,
+  Typography,
+  List,
+  ListItem,
+  ListItemText,
+  Button,
+} from "@mui/material";
+import { ShoppingCart, CheckCircleOutline } from "@mui/icons-material";
+import { fetcher } from "../helpers";
 
-export default function CartPage() {
-  return (
-    <div>CartPage</div>
-  )
+const CartComponent = ({ products }) => {
+  const [cart,setCart] = useState({});
+  function fetchCartFromAPI(){
+    fetcher(`ecommerce/cart`)
+      .then((res)=>{
+        setCart(res.data.items);
+      })
+      .catch(err=>console.log(err))
 }
+useEffect(()=>{
+  fetchCartFromAPI();
+})
+console.log(cart)
+  return (
+    <Paper
+      elevation={3}
+      style={{
+        maxWidth: 400,
+        padding: 40,
+        margin: "auto",
+        marginTop: 50,
+        textAlign: "center",
+        background: "#ffffff",
+        borderRadius: 10,
+        boxShadow: "0px 4px 20px rgba(0, 0, 0, 0.1)",
+      }}
+    >
+      <Typography variant="h4" style={{ marginBottom: 20, color: "#000000" }}>
+        Beyoung Cart
+      </Typography>
+
+      {/* <List style={{ marginBottom: 20 }}>
+        {products.map((product, index) => (
+          <ListItem key={index}>
+            <ListItemText primary={product.name} secondary={`$${product.price}`} />
+          </ListItem>
+        ))}
+      </List> */}
+
+
+      <Button
+        variant="contained"
+        color="primary"
+        style={{
+          background: "#000000",
+          color: "#ffffff",
+          width: "80%",
+        }}
+        startIcon={<CheckCircleOutline />}
+      >
+        Place Order
+      </Button>
+    </Paper>
+  );
+};
+
+export default CartComponent;
